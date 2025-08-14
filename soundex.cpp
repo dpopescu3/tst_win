97f05d37c5fb2697ce8e1e6bafc811cf29c3828f
Student Name <student@example.com>
2025-08-14 16:46:59 -0700

    Auto-commit #67 from CS106B-A1-main at 2025-08-14 16:46:59

--- soundex.cpp
+++ soundex.cpp
@@ -148,6 +148,12 @@ PROVIDED_TEST("DRELL is in uppercase") {
     EXPECT_EQUAL(soundex("DRELL"), "D640");
 }
 
+PROVIDED_TEST("DRELL is in uppercase") {
+    EXPECT_EQUAL(soundex("DRELL"), "D640");
+}
+PROVIDED_TEST("DRELL is in uppercase") {
+    EXPECT_EQUAL(soundex("DRELL"), "D640");
+}
 PROVIDED_TEST("Liu has to be padded with zeros") {
     EXPECT_EQUAL(soundex("Liu"), "L000");
 }
